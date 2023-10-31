0fb79a21cce27d70b3519c171ef69921abfcf4b7
kennyatu23 <G00267173@atu.ie>
2023-10-31 14:10:59 +0000

    Notifications

---
+++ src/main/java/ie/atu/lab4notification/Lab4NotificationApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.lab4notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4NotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab4NotificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/lab4notification/Lab4NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.lab4notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab4NotificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
